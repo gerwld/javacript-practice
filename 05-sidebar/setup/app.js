2b9bb73102a04295c665db56c1822a5d4e4adc67
Patryk Jaworski <pjaworski.dev@gmail.com>
2022-02-02 18:03:08 +0100

    Resolved project-5(anoter simple toggle)

---
+++ 05-sidebar/setup/app.js
@@ -0,0 +1,11 @@
+const sideBtn = document.querySelector('.sidebar-toggle');
+const sidebar = document.querySelector('.sidebar');
+const sidebarClose = document.querySelector('.close-btn');
+
+sideBtn.addEventListener('click', function(){
+    sidebar.classList.toggle('show-sidebar');
+});
+
+sidebarClose.addEventListener('click', function(){
+    sidebar.classList.remove('show-sidebar');
+});
